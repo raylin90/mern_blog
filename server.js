3cadadf32d5012fe4bde14fdd699a2a66e94ca78
Ray Lin <65415822+raylin90@users.noreply.github.com>
2021-11-04 16:06:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const port = 8000;
 // cookie allows us to create, read, work with cookie, which we used for Auth
 const cookies = require('cookie-parser');
 
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'build')));
-
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // // // // // // // // // // // // // // // // // // // // // // // // // // // //
