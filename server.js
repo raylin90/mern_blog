083282bff14ec4fe70a1cd115d850045a64731b3
Ray Lin <65415822+raylin90@users.noreply.github.com>
2021-11-04 15:58:25 -0400

    try deployment

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const port = 8000;
 // cookie allows us to create, read, work with cookie, which we used for Auth
 const cookies = require('cookie-parser');
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 // // // // // // // // // // AUTH W/ COOKIE // // // // // // // // // // // // //
